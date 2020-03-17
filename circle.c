9b3df3e3b5fbe0c863dcfc0dc548bcd5168490f8
Abdeali515253 <58637193+Abdeali515253@users.noreply.github.com>
2020-03-17 14:21:08 +0500

    Update circle.c

--- circle.c
+++ circle.c
@@ -1,18 +1,23 @@
-#include<stdio.h>
+#include<iostream>
 #include<math.h>
-int main(){
+using namespace std;
+main()
+{
 	int r = 10,y,x,d=2*r;
 	int circle[d][d];
-	for(y=0;y<d;y++){
-		for(x=0;x<d;x++){
+	for(y=0;y<d;y++)
+	{
+		for(x=0;x<d;x++)
+		{
 			circle[y][x] = 0;
 		}
 	}
-	for(x=0;x<r;x++){
+	for(x=0;x<r;x++)
+	{
 		y = round(sqrt(pow(r,2)-pow(x,2)));
 		circle[y+r-1][x+r-1]+=1;
 		circle[x+r-1][y+r-1]+=1;
-		circle[r-y4s4do bnbnf54rde6t5sf	d3xwde][r-x]+=1;
+		circle[r-y][r-x]+=1;
 		circle[r-x][r-y]+=1;
 		circle[y+r-1][r-x]+=1;
 		circle[x+r-1][r-y]+=1;
@@ -22,13 +27,13 @@ int main(){
 	for(y=0;y<d;y++){
 		for(x=0;x<d;x++){
 			if(circle[y][x]>=1){
-				printf("* ");
+				cout<<"* ";
 			}
 			else{
-				printf("  ");
+				cout<<"  ";
 			}
-			//printf("%d",circle[y][x]);
+			//cout<<circle[y][x];
 		}
-		printf("\n");
+		cout<<endl;
 	}
 }
